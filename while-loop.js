9ae204e0b3197e1c2f5d1b153f2eab5d2c5505b4
jubaer2222 <139365401+jubaer2222@users.noreply.github.com>
2025-05-30 20:04:18 +0600

    added few task about while loop

--- while-loop.js
+++ while-loop.js
@@ -1,7 +1,60 @@
-let count = 1;
+/*let count = 1;
 
 while(count < 5){
     
     console.log(count);
     count ++;
+}*/
+            //    print num 1 to 10
+/*let num = 1;
+while (num <=10){
+    console.log(num);
+    num++;
+}*/
+
+             // print 50 to 100
+/*
+let num = 50;
+while(num <=100){
+    console.log(num);
+    // num++;
 }
+    */
+
+            //  addition of num 1 to 10
+/*            
+let num = 1;
+let total = 0;
+while(num <=10){
+    console.log(num);
+    total =total + num;
+    console.log("total",total);
+    num++;
+    
+}*/
+
+        //    while loop use kore prothome 1 theke 10 porjonto print korbe . tarpor ei 1 theke 10 er addition dekhabe
+let num = 1;
+let total = 0;
+while(num <=10){
+    console.log(num);
+    total =total + num;
+    
+    num++;
+    
+}
+  console.log("total",total);
+        
+
+
+
+            // even numbers in 1 to 10
+/*let num = 1;
+while(num <= 10){
+    console.log(num);
+
+    if(num % 2 === 0){
+        console.log('even number',num);
+    }
+    num++;
+}  */          
